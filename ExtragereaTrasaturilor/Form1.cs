1acc0c37c20fbd57d501b84d32885d5b1cbf7950
andumihailescu <contact.andumihailescu@gmail.com>
2023-11-11 18:07:59 +0200

    Added globalVector

--- ExtragereaTrasaturilor/Form1.cs
+++ ExtragereaTrasaturilor/Form1.cs
@@ -1,22 +1,42 @@
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using System.Xml;
+using WordFrequency;
 
 namespace ExtragereaTrasaturilor
 {
     public partial class Form1 : Form
     {
+        private PorterStemmer porterStemmer = new PorterStemmer();
+        private string testingFolderPath = "D:\\School\\Data Mining\\ExtragereaTrasaturilor\\Reuters\\Reuters_34\\Testing";
+        private string trainingFolderPath = "D:\\School\\Data Mining\\ExtragereaTrasaturilor\\Reuters\\Reuters_34\\Training";
+        private string stopWordsFilePath = "D:\\School\\Data Mining\\ExtragereaTrasaturilor\\ExtragereaTrasaturilor\\stopwords.txt";
+        private List<string> stopWordsList;
+        private List<string> globalVector = new();
+
         public Form1()
         {
             InitializeComponent();
 
+            stopWordsList = LoadStopWords(stopWordsFilePath);
+
             OpenFiles();
+        }
+
+        private void CreateGlobalVector(List<string> words)
+        {
+            foreach (string word in words) 
+            { 
+                if (!globalVector.Contains(word))
+                {
+                    globalVector.Add(word);
+                }
+            }
         }
 
         public void OpenFiles()
         {
-            string testingFolderPath = "D:\\School\\Data Mining\\ExtragereaTrasaturilor\\Reuters\\Reuters_34\\Testing";
-            string trainingFolderPath = "D:\\School\\Data Mining\\ExtragereaTrasaturilor\\Reuters\\Reuters_34\\Training";
-
+            
 
             string folderPath = testingFolderPath;
 
@@ -32,6 +52,11 @@ namespace ExtragereaTrasaturilor
                     List<string> titleWords = ExtractWordsFromElement(xmlDoc, "//title");
                     List<string> textWords = ExtractWordsFromElement(xmlDoc, "//text");
                     List<string> codesWords = ExtractCodeAttributesFromElement(xmlDoc, "//codes[@class='bip:topics:1.0']/code");
+
+                    CreateGlobalVector(titleWords);
+                    CreateGlobalVector(textWords);
+                    CreateGlobalVector(codesWords);
+
 
                     DisplayWords("Title Words", titleWords);
                     DisplayWords("Text Words", textWords);
@@ -58,6 +83,10 @@ namespace ExtragereaTrasaturilor
                     List<string> textWords = ExtractWordsFromElement(xmlDoc, "//text");
                     List<string> codesWords = ExtractCodeAttributesFromElement(xmlDoc, "//codes[@class='bip:topics:1.0']/code");
 
+                    CreateGlobalVector(titleWords);
+                    CreateGlobalVector(textWords);
+                    CreateGlobalVector(codesWords);
+
                     DisplayWords("Title Words", titleWords);
                     DisplayWords("Text Words", textWords);
                     DisplayWords("Codes Words", codesWords);
@@ -74,7 +103,7 @@ namespace ExtragereaTrasaturilor
             // Select the specified elements using XPath
             XmlNodeList elements = xmlDoc.SelectNodes(xpath);
 
-            // Extract words from each selected element
+            // Extract globalVector from each selected element
             List<string> words = new List<string>();
             foreach (XmlNode element in elements)
             {
@@ -107,17 +136,22 @@ namespace ExtragereaTrasaturilor
 
         public List<string> ExtractWords(string text)
         {
-            // Use a regular expression to extract words (assumes words are alphanumeric and may contain apostrophes)
+            // Use a regular expression to extract globalVector (assumes globalVector are alphanumeric and may contain apostrophes)
             Regex regex = new Regex(@"\b[A-Za-z]+\b");
 
-            // Match words in the text
+            // Match globalVector in the text
             MatchCollection matches = regex.Matches(text);
 
             // Convert matches to a list of strings
             List<string> words = new List<string>();
             foreach (Match match in matches)
             {
-                words.Add(match.Value);
+                string word = match.Value.ToLower();
+                if (!stopWordsList.Contains(word))
+                {
+                    words.Add(porterStemmer.StemWord(word));
+                    //globalVector.Add(match.Value);
+                }
             }
 
             return words;
@@ -131,6 +165,20 @@ namespace ExtragereaTrasaturilor
                 Console.WriteLine($"- {word}");
             }
             Console.WriteLine();
+        }
+
+        static List<string> LoadStopWords(string filePath)
+        {
+            // Read stopwords from file and return as a list
+            try
+            {
+                return File.ReadAllLines(filePath).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading stopwords: {ex.Message}");
+                return new List<string>();
+            }
         }
     }
 }
